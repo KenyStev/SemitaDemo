4f49e6510d53baf5b1eb6fcfe08ce617635c93a4
Zero Kull <kjestevez.0795@gmail.com>
2014-06-20 15:40:27 -0600

    Ultima update de Semita

---
+++ src/edu/dhbw/andar/nav/Jugador2.java
@@ -0,0 +1,117 @@
+package edu.dhbw.andar.nav;
+
+import javax.microedition.khronos.opengles.GL10;
+
+import edu.dhbw.andar.pub.Global;
+import edu.dhbw.andobjviewer.graphics.FuncionesNaves;
+import edu.dhbw.andobjviewer.graphics.MiCharacter;
+
+public class Jugador2 extends MiCharacter implements FuncionesNaves{
+	
+	static String model_str = "navealienigena.obj";
+	static String pattern_str = "Rampano.patt";
+	public Bala bala;
+	boolean moveY = false;
+	
+	public Jugador2()
+	{
+		super(Global.getModel(model_str),pattern_str);
+	}
+	
+	public String getPattName(){
+		return pattern_str;
+	}
+
+	@Override
+	public void mover(int x, int y) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void armaUno() {
+		this.bala = new Bala(pattern_str);
+		try{
+			Global.artoolkit.registerARObject(this.bala);
+			this.bala.model.setYrot(-90f);
+		}catch(Exception e){
+			
+		}
+		
+	}
+
+	@Override
+	public void armaDos() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void armaTres() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void armaCuatro() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void armaCinco() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void armaSeis() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void kamikaze() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void specialBuff() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void moverY() {
+		// TODO Auto-generated method stub
+		if (moveY==false){
+			moveY=true;
+		}
+	}
+	
+	@Override
+	public void draw(GL10 gl){
+		super.draw(gl);
+		
+		if(this.hp.current<=0){
+			this.model.scale=0;
+		}
+		
+		if(moveY){
+			this.model.setYpos(-1f);
+			
+			if(this.model.ypos<-20){
+				moveY = false;
+			}
+		}else{
+			this.model.ypos=0f;
+		}
+		
+		if(this.moveY && Global.jugador1.moveY){
+			if(this.model.ypos<=-20 || Global.jugador1.model.ypos<=-20){
+				this.hp.decrese(Global.jugador1.hp.current);
+			}
+		}
+	}
+}
